b7de09a286331808e2a3032c6ded8d67875efe91
Dylan <55927823+lvlDylan@users.noreply.github.com>
2025-12-02 22:38:20 +0100

    Add prototype of portfolio

---
+++ assets/scripts/theme.js
@@ -0,0 +1,28 @@
+const savedTheme = localStorage.getItem('theme');
+const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+if (savedTheme) {
+    document.documentElement.setAttribute('data-bs-theme', savedTheme);
+} else {
+    document.documentElement.setAttribute('data-bs-theme', systemPrefersDark ? 'dark' : 'light');
+}
+
+function switchTheme() {
+    const htmlElement = document.documentElement;
+    const currentTheme = htmlElement.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    htmlElement.setAttribute('data-bs-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    switchTheme();
+});
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        navLinks.forEach(nav => nav.classList.remove('active'));
+        e.target.classList.add('active');
+    })
+})
---
+++ assets/scripts/types.js
@@ -0,0 +1,10 @@
+const typed = new Typed('#element-tapez', {
+    strings: ['Créatif.', 'Passionné.', 'Investi.'], // Tes textes ici
+    typeSpeed: 60,
+    backSpeed: 30,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+});
