02f255e0062c73e50921e3c51cc8142d47fea204
Dylan Lavieille <55927823+lvlDylan@users.noreply.github.com>
2026-01-16 23:12:56 +0100

    Modifying scroll comportement
    - Updating projects descriptions
    -  Updating typed text.

--- assets/scripts/types.js
+++ assets/scripts/types.js
@@ -1,14 +1,15 @@
 new Typed('#typing-text', {
     strings: [
-        "développeur junior.",
-        "passionné par le back-end et la programmation logicielle.",
-        "un futur développeur.",
-        "étudiant en BTS SIO."
+        "créateur de solutions logicielles.",
+        "développeur Back-End.",
+        "passionné par le Java et le Node.js.",
+        "étudiant en BTS SIO.",
     ],
-    typeSpeed: 60,
-    backSpeed: 30,
+    typeSpeed: 40,
+    backSpeed: 25,
     loop: true,
-    backDelay: 1500,
+    backDelay: 2000,
+    startDelay: 500,
     showCursor: true,
     cursorChar: '|',
 });
